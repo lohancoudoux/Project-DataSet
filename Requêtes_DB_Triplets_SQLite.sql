2ff3a27558c05b43e2b2594005783f63cb3e574d
Lohan COUDOUX <110205589+lohancoudoux@users.noreply.github.com>
2023-04-26 14:09:51 +0200

    Update Requêtes_DB_Triplets_SQLite.sql

--- Requêtes_DB_Triplets_SQLite.sql
+++ Requêtes_DB_Triplets_SQLite.sql
@@ -1,83 +1,71 @@
 -- SQLite
---SELECT * FROM Positive WHERE id2 BETWEEN 10 AND 20;
+SELECT * FROM Positive WHERE id2 BETWEEN 10 AND 20;
 
---SELECT DISTINCT id1 FROM Ancre;
+SELECT DISTINCT id1 FROM Ancre;
 
-/* SELECT a.anchor, p.pos, n.neg 
+SELECT a.anchor, p.pos, n.neg 
 FROM Ancre a 
 INNER JOIN Positive p ON a.id1 = p.id2 
-INNER JOIN Negative n ON a.id1 = n.id3 */
+INNER JOIN Negative n ON a.id1 = n.id3
 
-/*SELECT * FROM Ancre 
-WHERE anchor LIKE '%123%'*/
+SELECT * FROM Ancre 
+WHERE anchor LIKE '%123%'
 
-/*SELECT *
+SELECT * FROM Ancre
+WHERE id1 IN (SELECT id2 FROM Positive)
+AND id1 NOT IN (SELECT id3 FROM Negative);
+
+SELECT *
 FROM Ancre
 WHERE id1 IN (
   SELECT id2
   FROM Positive
 )
-AND id1 NOT IN (
-  SELECT id3
-  FROM Negative
-)*/
+AND anchor LIKE '%75%';
 
-/* SELECT *
-FROM Ancre
-WHERE id1 IN (
-  SELECT id2
-  FROM Positive
-)
-AND anchor LIKE '%75%' */
-
-/*SELECT id3, COUNT(*) AS count_negative
+SELECT id3, COUNT(*) AS count_negative
 FROM Negative
-GROUP BY id3;*/
+GROUP BY id3;
 
-/*SELECT Ancre.anchor, Positive.pos, Negative.neg 
+SELECT Ancre.anchor, Positive.pos, Negative.neg 
 FROM Ancre 
 INNER JOIN Positive ON Ancre.id1 = Positive.id2 
 INNER JOIN Negative ON Ancre.id1 = Negative.id3 
-GROUP BY Ancre.id1 */
+GROUP BY Ancre.id1;
 
-/* SELECT Ancre.anchor, COUNT(*) 
+SELECT Ancre.anchor, COUNT(*) 
 FROM Ancre 
 LEFT JOIN Positive ON Ancre.id1 = Positive.id2 
 LEFT JOIN Negative ON Ancre.id1 = Negative.id3 
 GROUP BY Ancre.anchor 
-HAVING COUNT(Positive.id2) > COUNT(Negative.id3) */ 
+HAVING COUNT(Positive.id2) > COUNT(Negative.id3)
 
-/* SELECT anchor FROM Ancre
+SELECT anchor FROM Ancre
 WHERE id1 IN (
   SELECT id2 FROM Positive
   UNION
-  SELECT id3 FROM Negative
-); */
+  SELECT id3 FROM Negative);
 
-/* SELECT anchor FROM Ancre
+SELECT anchor FROM Ancre
 WHERE id1 IN (
   SELECT id2 FROM Positive
 ) AND id1 IN (
-  SELECT id3 FROM Negative
-); */
+  SELECT id3 FROM Negative); 
 
-/*SELECT anchor FROM Ancre
+SELECT anchor FROM Ancre
 WHERE NOT EXISTS (
   SELECT * FROM Positive
   WHERE NOT EXISTS (
     SELECT * FROM Negative
     WHERE Negative.id3 = Positive.id2
-    AND Ancre.id1 = Positive.id2
-  )
-);*/
+    AND Ancre.id1 = Positive.id2));
 
-/*SELECT anchor FROM Ancre
-WHERE id1 <= ALL (SELECT id2 FROM Positive);*/
+SELECT anchor FROM Ancre
+WHERE id1 <= ALL (SELECT id2 FROM Positive);
 
-/*SELECT anchor FROM Ancre
+SELECT anchor FROM Ancre
 WHERE id1 = ANY (
-  SELECT id3 FROM Negative
-);*/
+  SELECT id3 FROM Negative);
 
 
 
